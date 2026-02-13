0ba41263a9714d16519eef0f0703f874b6a97ab4
KumarAryan01 <Kumararyan86039@gmail.com>
2026-02-14 00:42:38 +0530

    Initial commit

---
+++ README02.txt
@@ -0,0 +1,32 @@
+Compilation :
+g++ assg02.cpp -o assg02
+
+Input File Format
+Each assignment is given as:
+
+A <id> <prompt-count> <dependencies> 0
+
+Execution Format
+
+assg02 <input-file> <mode> <N> <K/M>
+
+N  = number of students
+K  = number of prompts per student per day
+M  = number of days
+
+Modes
+Mode 1:
+Given N and K, find minimum days required.
+assg02 <input-file> 1 N K
+
+Mode 2:
+Given N and M, find minimum prompts per student.
+assg02 <input-file> 2 N M
+
+Mode 3:
+Same as Mode 1 but with 6am solution-sharing delay.
+assg02 <input-file> 3 N K
+
+Mode 4:
+Same as Mode 2 but with 6am solution-sharing delay.
+assg02 <input-file> 4 N M
---
+++ assg02.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Task {
+    int id;
+    int cost;
+    vector<int> deps;
+};
+
+vector<Task> tasks;
+unordered_map<int, vector<int>> graph;
+unordered_map<int, int> indeg;
+unordered_map<int, int> need;
+
+bool canFinish(int students, int K, int days, bool delay) {
+
+    queue<int> q;
+    unordered_map<int,int> deg = indeg;
+    vector<vector<int>> nextDay(days + 2);
+
+    for(auto &t : tasks)
+        if(deg[t.id] == 0)
+            q.push(t.id);
+
+    int done = 0;
+
+    for(int day = 1; day <= days; day++) {
+
+        vector<int> left(students, K);
+        queue<int> temp;
+
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            bool assigned = false;
+
+            for(int i = 0; i < students; i++) {
+                if(left[i] >= need[cur]) {
+                    left[i] -= need[cur];
+                    assigned = true;
+                    done++;
+
+                    for(int nxt : graph[cur]) {
+                        deg[nxt]--;
+                        if(deg[nxt] == 0) {
+                            if(delay) nextDay[day + 1].push_back(nxt);
+                            else q.push(nxt);
+                        }
+                    }
+                    break;
+                }
+            }
+
+            if(!assigned)
+                temp.push(cur);
+        }
+
+        q = temp;
+        for(int x : nextDay[day])
+            q.push(x);
+    }
+
+    return done == tasks.size();
+}
+
+int main(int argc, char** argv) {
+    if(argc < 5) {
+        cout << "Usage: assg02 <input-file> <mode> <N> <K/M>\n";
+        return 0;
+    }
+
+    string file = argv[1];
+    int mode = stoi(argv[2]);
+    int students = stoi(argv[3]);
+    int val = stoi(argv[4]);
+    bool delay = (mode >= 3);
+
+    ifstream in(file);
+    string s;
+    while(in >> s) {
+        if(s == "%") {
+            string t; getline(in,t);
+        } else if(s == "A") {
+            Task t;
+            in >> t.id >> t.cost;
+            need[t.id] = t.cost;
+            int d;
+            while(in >> d && d != 0) {
+                t.deps.push_back(d);
+                graph[d].push_back(t.id);
+                indeg[t.id]++;
+            }
+            tasks.push_back(t);
+        }
+    }
+
+    if(mode == 1 || mode == 3) {
+        int K = val;
+        for(int d = 1; ; d++) {
+            if(canFinish(students, K, d, delay)) {
+                cout << "Minimum days = " << d << "\n";
+                break;
+            }
+        }
+    }
+    else {
+        int days = val;
+        int lo = 1, hi = 0;
+        for(auto &t : tasks) hi += t.cost;
+
+        int ans = hi;
+        while(lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if(canFinish(students, mid, days, delay)) {
+                ans = mid;
+                hi = mid - 1;
+            } else lo = mid + 1;
+        }
+
+        cout << "Minimum prompts per student = " << ans << "\n";
+    }
+}
